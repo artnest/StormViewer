ce29c3902007d60dd33cf6be1359c8c7bdec6887
artnest <artnest789@gmail.com>
2020-02-12 10:44:03 +0300

    Initial commit

---
+++ Storm Viewer/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Artyom Nesterenko on 7/1/20.
+//  Copyright © 2020 artnest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+}
+
